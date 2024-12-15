4e7675e7364b05854cd9ffa82f4f2f75a1a0c757
TristanFraganud <tristan.fragnaud@epitech.eu>
2024-12-15 11:38:36 +0100

    fix: pause menu and add bg to start_menu.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -3,18 +3,30 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    public bool pause = false;
     [SerializeField] GameObject pauseMenu;
     
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) {
-            Time.timeScale = 0f;
-            pauseMenu.SetActive(true);
+            Put_pause();
         }
     }
     
+    void Put_pause()
+    {
+        if (pause){
+            Continue();
+        } else {
+            pause = true;
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
     public void Continue()
     {
+        pause = false;
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
     }
